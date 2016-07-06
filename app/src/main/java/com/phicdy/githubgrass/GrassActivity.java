80247b431404517c380eec4b1640e638daf19e1a
phicdy <phicdy@gmail.com>
2016-07-07 00:02:43 +0900

    Add project

---
+++ app/src/main/java/com/phicdy/githubgrass/GrassActivity.java
@@ -0,0 +1,13 @@
+package com.phicdy.githubgrass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GrassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grass);
+    }
+}
